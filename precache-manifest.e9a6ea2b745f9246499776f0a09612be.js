d9c0979e927db53c10fc8b41138635f7eed87853
vlepekha <vlepekha@student.ucode.world>
2021-07-16 17:43:06 +0300

    Updates

---
+++ precache-manifest.e9a6ea2b745f9246499776f0a09612be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1add5e98fb251d69dd9b693492868c9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c11fe58f970f94a8b717",
+    "url": "./static/css/main.bad156a5.chunk.css"
+  },
+  {
+    "revision": "486876fe5217784983bf",
+    "url": "./static/js/2.79d301ac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.79d301ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c11fe58f970f94a8b717",
+    "url": "./static/js/main.cbeff4bb.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
